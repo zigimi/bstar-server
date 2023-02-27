b35db354f27acf18b740d9ecb32a46afa13fbaf8
jhcheon <jhcheon@realsecu.net>
2023-02-27 17:29:53 +0900

    수정

--- src/main/java/com/bstarbackend/bstar/BstarApplication.java
+++ src/main/java/com/bstarbackend/bstar/BstarApplication.java
@@ -2,6 +2,7 @@ package com.bstarbackend.bstar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
@@ -9,7 +10,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class BstarApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BstarApplication.class, args);
+		SpringApplication application = new SpringApplication(BstarApplication.class);
+		application.addListeners(new ApplicationPidFileWriter());
+		application.run(args);
 	}
 
 }
