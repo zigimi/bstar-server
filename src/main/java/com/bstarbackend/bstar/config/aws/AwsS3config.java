cb34b8d5267f8a9a42558cc721ded6c9681bc2b5
jhcheon <jhcheon@realsecu.net>
2023-02-28 16:42:33 +0900

    s3 이미지 업로드용 수정

--- src/main/java/com/bstarbackend/bstar/config/aws/AwsS3config.java
+++ src/main/java/com/bstarbackend/bstar/config/aws/AwsS3config.java
@@ -13,10 +13,10 @@ public class AwsS3config {
     @Configuration
     public class AwsS3Config {
 
-        @Value("${cloud.aws.credentials.access-key}")
+        @Value("${cloud.aws.credentials.accessKey}")
         private String accessKey;
 
-        @Value("${cloud.aws.credentials.secret-key}")
+        @Value("${cloud.aws.credentials.secretKey}")
         private String secretKey;
 
         @Value("${cloud.aws.region.static}")
